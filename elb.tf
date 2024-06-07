f183883c7a621f66bef5a1dc8ff637945989e876
nagswe <126598198+nagswe@users.noreply.github.com>
2024-06-07 23:30:18 +0530

    Update elb.tf
    yes

---
+++ elb.tf
@@ -0,0 +1,27 @@
+
+resource "aws_elb" "bar" {
+  name               = "nagesh-terraform-elb"
+  availability_zones = ["eu-west-2a", "eu-west-2b"]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 5
+    target              = "HTTP:80/"
+    interval            = 30
+  }
+
+  instances                 = ["${aws_instance.one.id}", "${aws_instance.two.id}"]
+  cross_zone_load_balancing = true
+  idle_timeout              = 400
+  tags = {
+    Name = "nagesh-tf-elb"
+  }
+}
